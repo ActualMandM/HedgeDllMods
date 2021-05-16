616088c7936a11b57e4d1a2ef101eb44d7b53b4f
M&M <goldtextwitch@outlook.com>
2021-05-16 09:47:23 -0700

    [Link Sonic] Remove check for PBR Shaders
    No longer needed as of https://github.com/blueskythlikesclouds/GenerationsPBRShaders/commit/ad00c9a285835b85c6698c6715a08188e8ba5226

--- Source/LinkSonic/Mod.cpp
+++ Source/LinkSonic/Mod.cpp
@@ -2,12 +2,8 @@
 
 extern "C" __declspec(dllexport) void Init()
 {
+	ArchiveTreePatcher::applyPatches();
+
 	// Disable Title Loading Video by Hyper
 	WRITE_MEMORY(0xD6966E, uint8_t, 0xE9, 0x14, 0x01, 0x00, 0x00);
-}
-
-extern "C" __declspec(dllexport) void PostInit()
-{
-	if (GetModuleHandle(TEXT("GenerationsPBRShaders.dll")) == nullptr)
-		ArchiveTreePatcher::applyPatches();
 }
