e2e960a1ccfbda989fc8281b46c4896d62d82174
M&M <goldtextwitch@outlook.com>
2021-05-24 10:07:17 -0700

    [Link Sonic] refactor Mod.cpp

--- Source/LinkSonic/Mod.cpp
+++ Source/LinkSonic/Mod.cpp
@@ -1,32 +1,14 @@
 #include "ArchiveTreePatcher.h"
 #include "Configuration.h"
 
-extern "C" __declspec(dllexport) void Init(ModInfo* modInfo)
+extern "C" __declspec(dllexport) void Init()
 {
 	// Disable Title Loading Video by HyperBE32
 	WRITE_MEMORY(0xD6966E, uint8_t, 0xE9, 0x14, 0x01, 0x00, 0x00);
 
 	// read configuration file
-	std::string dir = modInfo->CurrentMod->Path;
-
-	size_t pos = dir.find_last_of("\\/");
-	if (pos != std::string::npos)
-		dir.erase(pos + 1);
-
-	if (Configuration::load(dir + "LinkSonic.ini"))
-		return;
-
-	MessageBox(nullptr, TEXT("Failed to load the config file!\nPlease make sure that LinkSonic.ini exists in the mod's folder."), TEXT("Link Sonic"), MB_ICONERROR);
-}
-
-extern "C" __declspec(dllexport) void PostInit(ModInfo* mods)
-{
-	// check for loaded mods
-	for (Mod* mod : *mods->ModList)
-	{
-		if (!_stricmp(mod->Name, "eggmanland"))
-			ArchiveTreePatcher::m_archiveDependencies.push_back(ArchiveDependency("LinkSonicEGB", { "cpz200" }));
-	}
+	if (!Configuration::load("LinkSonic.ini"))
+		MessageBox(nullptr, TEXT("Failed to load the config file!\nPlease make sure that LinkSonic.ini exists in the mod's folder."), TEXT("Link Sonic"), MB_ICONERROR);
 
 	// TunicType configuration
 	switch (Configuration::tunicType)
@@ -46,6 +28,16 @@ extern "C" __declspec(dllexport) void PostInit(ModInfo* mods)
 		default:
 			ArchiveTreePatcher::m_archiveDependencies.push_back(ArchiveDependency("AppearanceGreen", { "LinkSonic", "LinkSonicEGB", "PBRSonic" }));
 			break;
+	}
+}
+
+extern "C" __declspec(dllexport) void PostInit(ModInfo* mods)
+{
+	// check for loaded mods
+	for (Mod* mod : *mods->ModList)
+	{
+		if (!_stricmp(mod->Name, "eggmanland"))
+			ArchiveTreePatcher::m_archiveDependencies.push_back(ArchiveDependency("LinkSonicEGB", { "cpz200" }));
 	}
 
 	ArchiveTreePatcher::applyPatches();
