b831009cbc4e6519b657040b24c540ae903a6df1
HyperBE32 <HyperPolygon64@gmail.com>
2021-05-27 20:44:15 +0100

    [Sonic Colors] Disabled more voice clips

--- Source/ColorsSonic/Mod.cpp
+++ Source/ColorsSonic/Mod.cpp
@@ -21,29 +21,36 @@ extern "C" __declspec(dllexport) void Init()
 		WRITE_MEMORY(0x1277C0E, uint32_t, 0x015E6859);
 
 		// Disable Character Action Voices by HyperBE32
+		WRITE_MEMORY(0xDF0B96, uint32_t, 0xFFFFFF); /* 3002_action01 (used for Unleashed jump selector) */
+		WRITE_MEMORY(0xDF0C08, uint32_t, 0xFFFFFF); /* 3002_action01 (used for Unleashed jump selector) */
 		WRITE_MEMORY(0xDFC993, uint32_t, 0xFFFFFF); /* 3002_action01 (used for bouncing off physics objects) */
 		WRITE_MEMORY(0xE4B684, uint32_t, 0xFFFFFF); /* 3002_action01 (used for tricks) */
 		WRITE_MEMORY(0xE4B8E0, uint32_t, 0xFFFFFF); /* 3002_action01 (used for trick finish) */
 		WRITE_MEMORY(0xE57E6F, uint32_t, 0xFFFFFF); /* 3002_action01 (used for jumping) */
 		WRITE_MEMORY(0x11D7B8C, uint32_t, 0xFFFFFF); /* 3002_action01 (used for grinding) */
+		WRITE_MEMORY(0x123B475, uint32_t, 0xFFFFFF); /* 3002_action01 (used for cannons) */
+		WRITE_MEMORY(0x1252740, uint32_t, 0xFFFFFF); /* 3002_action01 (used for squat kick) */
 		WRITE_MEMORY(0x1117EF4, uint32_t, 0xFFFFFF); /* 3002_stumble */
 		WRITE_MEMORY(0xE0248C, uint32_t, 0xFFFFFF); /* 3002_damage01 */
 		WRITE_MEMORY(0xE02496, uint32_t, 0xFFFFFF); /* 3002_damage02 */
 		WRITE_MEMORY(0xE024A0, uint32_t, 0xFFFFFF); /* 3002_damage03 */
 		WRITE_MEMORY(0xE024AA, uint32_t, 0xFFFFFF); /* 3002_damage04 */
-		WRITE_MEMORY(0xDF0B63, uint32_t, 0xFFFFFF); /* 3002_damage06 */
+		WRITE_MEMORY(0xDF0B63, uint32_t, 0xFFFFFF); /* 3002_damage05 (used for Unleashed jump selector) */
+		WRITE_MEMORY(0xE4466E, uint32_t, 0xFFFFFF); /* 3002_strain (used for pushing) */
 		WRITE_MEMORY(0xE023E3, uint32_t, 0xFFFFFF); /* 3002_dead (used for dying from enemies) */
 		WRITE_MEMORY(0xE55D3D, uint32_t, 0xFFFFFF); /* 3002_dead (used for dying from falling) */
+		WRITE_MEMORY(0x1117CEB, uint32_t, 0xFFFFFF); /* 3002_action03 (used for boost) */
 		WRITE_MEMORY(0x1118512, uint32_t, 0xFFFFFF); /* 3002_action03 (used for drift) */
 		WRITE_MEMORY(0x1254E04, uint32_t, 0xFFFFFF); /* 3002_action03 (used for stomping) */
 		WRITE_MEMORY(0xDF3710, uint32_t, 0xFFFFFF); /* 3002_action04 (used for homing success) */
-		WRITE_MEMORY(0x1117CEB, uint32_t, 0xFFFFFF); /* 3002_action04 (used for boost) */
 		WRITE_MEMORY(0x1117D09, uint32_t, 0xFFFFFF); /* 3002_action04 (used for boost) */
-		WRITE_MEMORY(0x1117D27, uint32_t, 0xFFFFFF); /* 3002_action04 (used for boost) */
 		WRITE_MEMORY(0x11184E4, uint32_t, 0xFFFFFF); /* 3002_action04 (used for homing success) */
-		WRITE_MEMORY(0xE4466E, uint32_t, 0xFFFFFF); /* 3002_strain (used for pushing) */
+		WRITE_MEMORY(0x1119B1F, uint32_t, 0xFFFFFF); /* 3002_action04 (used for board drift) */
 		WRITE_MEMORY(0xE3DADD, uint32_t, 0xFFFFFF); /* 3002_success01 (used for blast off) */
 		WRITE_MEMORY(0xE462AA, uint32_t, 0xFFFFFF); /* 3002_success01 (used for pulleys) */
+		WRITE_MEMORY(0x1117D27, uint32_t, 0xFFFFFF); /* 3002_action05 (used for boost) */
+		WRITE_MEMORY(0x1231971, uint32_t, 0xFFFFFF); /* 3002_action05 (used for light dash) */
+		WRITE_MEMORY(0xDF0B63, uint32_t, 0xFFFFFF); /* 3002_damage06 */
 		WRITE_MEMORY(0x11D72DC, uint32_t, 0xFFFFFF); /* 3002_action06 */
 
 		switch (Configuration::simulatorType)
