bdf91f4dd9fe8612d80930f9eb72e4e5422cc6a5
HyperBE32 <HyperPolygon64@gmail.com>
2021-05-27 10:00:52 +0100

    Implemented patches to disable Sonic's voice for Sim Sonic This implementation isn't final and needs more work - see the to-do in the source file.

--- Source/ColorsSonic/Mod.cpp
+++ Source/ColorsSonic/Mod.cpp
@@ -20,6 +20,30 @@ extern "C" __declspec(dllexport) void Init()
 		WRITE_MEMORY(0x1277C75, uint32_t, 0x015E6848);
 		WRITE_MEMORY(0x1277C0E, uint32_t, 0x015E6859);
 
+		// Disable Character Action Voices by HyperBE32
+		// TODO: disable trick and speed up voices!
+		WRITE_MEMORY(0xDFC993, uint32_t, 0xFFFFFF); /* 3002_action01 (used for bouncing off physics objects) */
+		WRITE_MEMORY(0xE57E6F, uint32_t, 0xFFFFFF); /* 3002_action01 (used for jumping) */
+		WRITE_MEMORY(0x11D7B8C, uint32_t, 0xFFFFFF); /* 3002_action01 (used for grinding) */
+		WRITE_MEMORY(0x1117EF4, uint32_t, 0xFFFFFF); /* 3002_stumble */
+		WRITE_MEMORY(0xE0248C, uint32_t, 0xFFFFFF); /* 3002_damage01 */
+		WRITE_MEMORY(0xE02496, uint32_t, 0xFFFFFF); /* 3002_damage02 */
+		WRITE_MEMORY(0xE024A0, uint32_t, 0xFFFFFF); /* 3002_damage03 */
+		WRITE_MEMORY(0xE024AA, uint32_t, 0xFFFFFF); /* 3002_damage04 */
+		WRITE_MEMORY(0xDF0B63, uint32_t, 0xFFFFFF); /* 3002_damage06 */
+		WRITE_MEMORY(0xE023E3, uint32_t, 0xFFFFFF); /* 3002_dead (used for dying from enemies) */
+		WRITE_MEMORY(0xE55D3D, uint32_t, 0xFFFFFF); /* 3002_dead (used for dying from falling) */
+		WRITE_MEMORY(0x1118512, uint32_t, 0xFFFFFF); /* 3002_action03 (used for drift) */
+		WRITE_MEMORY(0x1254E04, uint32_t, 0xFFFFFF); /* 3002_action03 (used for stomping) */
+		WRITE_MEMORY(0xDF3710, uint32_t, 0xFFFFFF); /* 3002_action04 (used for homing success) */
+		WRITE_MEMORY(0x1117CEB, uint32_t, 0xFFFFFF); /* 3002_action04 (used for boost) */
+		WRITE_MEMORY(0x1117D09, uint32_t, 0xFFFFFF); /* 3002_action04 (used for boost) */
+		WRITE_MEMORY(0x1117D27, uint32_t, 0xFFFFFF); /* 3002_action04 (used for boost) */
+		WRITE_MEMORY(0x11184E4, uint32_t, 0xFFFFFF); /* 3002_action04 (used for homing success) */
+		WRITE_MEMORY(0xE4466E, uint32_t, 0xFFFFFF); /* 3002_strain (used for pushing) */
+		WRITE_MEMORY(0xE462AA, uint32_t, 0xFFFFFF); /* 3002_success01 (used for pulleys) */
+		WRITE_MEMORY(0x11D72DC, uint32_t, 0xFFFFFF); /* 3002_action06 */
+
 		switch (Configuration::simulatorType)
 		{
 			case SimulatorType::Blue:
