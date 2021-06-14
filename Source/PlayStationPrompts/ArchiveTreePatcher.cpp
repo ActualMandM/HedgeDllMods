a9ad3088fc9d0717a0fc2761ffabf67533832aa2
M&M <goldtextwitch@outlook.com>
2021-06-14 00:22:41 -0700

    [PlayStation Prompts] add a few more archives

--- Source/PlayStationPrompts/ArchiveTreePatcher.cpp
+++ Source/PlayStationPrompts/ArchiveTreePatcher.cpp
@@ -2,7 +2,7 @@
 
 const std::vector<std::pair<const char*, const std::vector<const char*>>> NODES =
 {
-	{ "DualShock3Common", {"SonicActionCommonHud", "SystemCommon", "cmn200", "Guide", "pam_cmn", "StageGate", "Title"}}
+	{ "DualShock3Common", {"SonicActionCommon", "SonicActionCommonHud", "SystemCommon", "cmn200", "Guide", "pam_cmn", "StageGate", "Title", "Network", "NoticeBoard"}}
 };
 
 HOOK(bool, __stdcall, ParseArchiveTree, 0xD4C8E0, void* A1, char* pData, const size_t size, void* pDatabase)
