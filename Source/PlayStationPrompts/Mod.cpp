054859a7a60d10a0f9ee9b478e0d99f3be0b5d76
M&M <goldtextwitch@outlook.com>
2021-06-14 11:09:06 -0700

    [PlayStation Prompts] patch the string in memory instead of patching the archive

--- Source/PlayStationPrompts/Mod.cpp
+++ Source/PlayStationPrompts/Mod.cpp
@@ -2,5 +2,8 @@
 
 extern "C" __declspec(dllexport) void Init()
 {
+	// Swap the image for "You can now switch between the two Sonics."
+	WRITE_MEMORY(0x15C5624, uint8_t, 0x70);
+
 	ArchiveTreePatcher::applyPatches();
 }
