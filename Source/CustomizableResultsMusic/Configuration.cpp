6aa952e614c47f76d7f47dbf62612d1f33025434
M&M <goldtextwitch@outlook.com>
2021-07-28 20:32:54 -0700

    [Customizable Results Music] Make use of already defined variable

--- Source/CustomizableResultsMusic/Configuration.cpp
+++ Source/CustomizableResultsMusic/Configuration.cpp
@@ -17,7 +17,7 @@ bool Configuration::load(const std::string& filePath)
 	songChoice = reader.Get("Main", "IncludeDir0", songChoice);
 	customDuration = reader.GetFloat("Config", "customDuration", customDuration);
 	customSingle = reader.GetBoolean("Config", "customSingle", customSingle);
-	SRankType = (SRank)reader.GetInteger("Config", "SRankType", SRank::Default);
+	SRankType = (SRank)reader.GetInteger("Config", "SRankType", SRankType);
 
 	return true;
 }
