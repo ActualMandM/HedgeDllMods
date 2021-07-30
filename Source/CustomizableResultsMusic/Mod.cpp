51f2f62a51bc271c31e066557c11493c17c6cb9a
M&M <goldtextwitch@outlook.com>
2021-07-30 11:17:48 -0700

    [Customizable Results Music] Push current code

--- Source/CustomizableResultsMusic/Mod.cpp
+++ Source/CustomizableResultsMusic/Mod.cpp
@@ -4,12 +4,45 @@
 #define ASMHOOK void __declspec(naked)
 
 
+const char* ResultsChar[] =
+{
+	"Custom",
+	"Sonic1",
+	"SCDJP",
+	"SCDUS",
+	"S3K",
+	"SonicR",
+	"Adventure",
+	"Adventure2",
+	"Shadow",
+	"Sonic06",
+	"Sonic06Town",
+	"SecretRings",
+	"Unleashed",
+	"UnleashedBoss",
+	"BlackKnight",
+	"S4E1",
+	"Colors",
+	"ColorsBoss",
+	"S4E2",
+	"LostWorld",
+	"Mania",
+	"Forces",
+	"SRB2",
+	"Persona",
+	"Persona2IS",
+	"Persona2EP",
+	"Persona3",
+	"Persona4",
+	"Persona5"
+};
+
 // This lets Classic have a different song than Modern
 // Set these to whatever you want in the config.
-const char* resultStringM = "Result";  // Change this to whatever string you want for Modern's music.
-const char* resultStringC = "1up";     // Change this to whatever string you want for Classic's music.
+char resultStringM[32];
+char resultStringC[32];
 
-uint32_t mhi_jump = 0x00CFD3CD;
+uint32_t cac_jump = 0x00CFD3CD;
 
 ASMHOOK CustomActClear()
 {
@@ -25,15 +58,95 @@ ASMHOOK CustomActClear()
 		jz jump
 
 		pop eax
-		push[resultStringM]
+		push offset resultStringM
 		jmp retrn
 
 		jump :
 		pop eax
-			push[resultStringC]
+		push offset resultStringC
+		nop
 
-			retrn :
-			jmp[mhi_jump]
+		retrn :
+		jmp[cac_jump]
+	}
+}
+
+
+void SRankResult(SRank Option)
+{
+	if (Option == Always)
+	{
+		WRITE_MEMORY(0xCFD4CA, uint8_t, 0xF8, 0x38);
+		WRITE_MEMORY(0xCFD4E8, uint8_t, 0xF8, 0x38);
+	}
+	else if (Option == Never)
+	{
+		WRITE_MEMORY(0xCFD4CA, uint8_t, 0x00, 0x39);
+		WRITE_MEMORY(0xCFD4E8, uint8_t, 0x00, 0x39);
+	}
+}
+
+// Swap for Result 1 - No S Rank
+char result1StringM[32];
+char result1StringC[32];
+
+uint32_t cr1_jump = 0x00CFD4CE;
+
+ASMHOOK CustomResult1()
+{
+	__asm
+	{
+		push eax
+		mov eax, 0x01E5E304 // Classic sonic singleton, is 0 if modern sonic.
+		cmp[eax], 0
+		jne jump
+
+		mov eax, 0x01E5E2F8 // Modern sonic singleton, is 0 if classic sonic.
+		cmp[eax], 0
+		jz jump
+
+		pop eax
+		push offset result1StringM
+		jmp retrn
+
+		jump :
+		pop eax
+		push offset result1StringC
+
+		retrn :
+		jmp[cr1_jump]
+	}
+}
+
+// Swap for Result 2 - S Rank
+char result2StringM[32];
+char result2StringC[32];
+
+uint32_t cr2_jump = 0x00CFD4EC;
+
+ASMHOOK CustomResult2()
+{
+	__asm
+	{
+		push eax
+		mov eax, 0x01E5E304 // Classic sonic singleton, is 0 if modern sonic.
+		cmp[eax], 0
+		jne jump
+
+		mov eax, 0x01E5E2F8 // Modern sonic singleton, is 0 if classic sonic.
+		cmp[eax], 0
+		jz jump
+
+		pop eax
+		push offset result2StringM
+		jmp retrn
+
+		jump :
+		pop eax
+		push offset result2StringC
+
+		retrn :
+		jmp[cr2_jump]
 	}
 }
 
@@ -55,7 +168,7 @@ ASMHOOK CustomResultsTime()
 		jne jump
 
 		mov eax, 0x01E5E2F8 // Modern sonic singleton, is 0 if classic sonic.
-		cmp[eax], 0         // Second pass of checking in case something terrible happens (youd be surprised)
+		cmp[eax], 0
 		jz jump
 
 		// Modern sonic duration
@@ -66,13 +179,13 @@ ASMHOOK CustomResultsTime()
 		// Classic sonic duration
 		jump :
 		lea eax, resultTimeC
-			movsd xmm1, [eax]
-			jmp retrn
+		movsd xmm1, [eax]
+		jmp retrn
 
 
-			retrn :
+		retrn :
 		pop eax
-			jmp[resultTimeJumpOut]
+		jmp[resultTimeJumpOut]
 	}
 }
 
@@ -93,37 +206,91 @@ void RoundClearDuration(float duration)
 	WRITE_MEMORY(0x17046C0, double, duration);
 }
 
+double SetResultTime(Results result)
+{
+	double outValue;
+	
+	switch (result)
+	{
+	case Sonic06:
+	case Sonic06Town:
+		outValue = 7.381;
+		break;
+	case Unleashed:
+	case UnleashedBoss:
+		outValue = 6.021;
+		break;
+	case Colors:
+	case ColorsBoss:
+		outValue = 8.01;
+		break;
+	case LostWorld:
+		outValue = 8.182;
+		break;
+	case BlackKnight:
+		outValue = 10.0;
+		break;
+	case Custom:
+		outValue = (double)Configuration::CustomDuration;
+		if (Configuration::OnlyRoundClear) OnlyRoundClear();
+		break;
+	default:
+		if(static_cast<int>(result) >= 0) OnlyRoundClear();
+		outValue = *(double*)0x017046C0;
+		break;
+	}
+	
+	return outValue;
+}
+
+void PrepareStrings(Results resultC, Results resultM)
+{
+	int resultClassic = (int)resultC;
+	int resultModern  = (int)resultM;
+
+	const char* resultsTypeC = resultClassic < 0 ? "" : ResultsChar[resultClassic];
+	const char* resultsTypeM = resultModern  < 0 ? "" : ResultsChar[resultModern ];
+
+	strcpy( resultStringC, resultClassic < 0 ? "Result"  : ("RC_" + std::string(resultsTypeC)).c_str());
+	strcpy(result1StringC, resultClassic < 0 ? "Result1" : ("R1_" + std::string(resultsTypeC)).c_str());
+	strcpy(result2StringC, resultClassic < 0 ? "Result2" : ("R2_" + std::string(resultsTypeC)).c_str());
+
+	strcpy( resultStringM, resultModern  < 0 ? "Result"  : ("RC_" + std::string(resultsTypeM)).c_str());
+	strcpy(result1StringM, resultModern  < 0 ? "Result1" : ("R1_" + std::string(resultsTypeM)).c_str());
+	strcpy(result2StringM, resultModern  < 0 ? "Result2" : ("R2_" + std::string(resultsTypeM)).c_str());
+	
+	switch (Configuration::SRankType)
+	{
+	case Always:
+		strcpy(result2StringC, resultClassic < 0 ? "Result1" : ("R1_" + std::string(resultsTypeC)).c_str());
+		strcpy(result2StringM, resultModern  < 0 ? "Result1" : ("R1_" + std::string(resultsTypeM)).c_str());
+		break;
+	case Never:
+		strcpy(result1StringC, resultClassic < 0 ? "Result2" : ("R2_" + std::string(resultsTypeC)).c_str());
+		strcpy(result1StringM, resultModern  < 0 ? "Result2" : ("R2_" + std::string(resultsTypeM)).c_str());
+		break;
+	}
+}
+
 extern "C" __declspec(dllexport) void Init()
 {
-	Configuration::load("mod.ini");
+	//MessageBoxA(NULL, "ALT + SHIFT + P \n(or CTRL+ALT+P on new launch)", "Attach to VS", MB_ICONWARNING);
+	//Configuration::load("mod.ini");
+	Configuration::load("CRM.ini");
 
-	/*
-	switch (Configuration::ResultOptionModern)
-	{
-		case S06:
-		case S06Town:
-			RoundClearDuration(7.381f);
-			break;
-		case Unleashed:
-		case UnleashedBoss:
-			RoundClearDuration(6.021f);
-			break;
-		case Colors:
-		case ColorsBoss:
-			RoundClearDuration(8.01f);
-			break;
-		case LostWorld:
-			RoundClearDuration(8.182f);
-			break;
-		case BlackKnight:
-			RoundClearDuration(10.0f);
-			break;
-		case Custom:
-			RoundClearDuration(Configuration::CustomDuration);
-			break;
-	}
-	*/
+	WRITE_JUMP(0x00CFD3C8, CustomActClear)
+	WRITE_JUMP(0x00CFD4C9, CustomResult1)
+	WRITE_JUMP(0x00CFD4E7, CustomResult2)
+	WRITE_JUMP(0x00CFD55E, CustomResultsTime)
+}
 
-	WRITE_JUMP(0x00CFD3C8, CustomActClear);
-	WRITE_JUMP(0x00CFD55E, CustomResultsTime);
+extern "C" __declspec(dllexport) void PostInit()
+{
+	const Results resultC = Configuration::ResultOptionClassic;
+	const Results resultM = Configuration::ResultOptionModern;
+
+ 	PrepareStrings(resultC, resultM);
+
+	resultTimeC = SetResultTime(resultC);
+	resultTimeM = SetResultTime(resultM);
 }
