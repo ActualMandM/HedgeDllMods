69a3a7d3251ca7312e6085682020df546538c848
Michael <15317421+ActualMandM@users.noreply.github.com>
2022-05-09 15:52:12 -0700

    [OopsAllDiffuse] Add source code.
    Co-authored-by: Skyth <19259897+blueskythlikesclouds@users.noreply.github.com>

---
+++ Source/bb/OopsAllDiffuse/Mod.cpp
@@ -0,0 +1,23 @@
+// Defined here so we don't need to re-write this all the time.
+#define ASMHOOK void __declspec(naked)
+
+uint32_t midAsmHookFunctionAddress = 0x6621A0;
+uint32_t midAsmHookReturnAddress = 0x74251B;
+
+const char* volatile const shader = "SysDiffuse";
+
+ASMHOOK replaceShader()
+{
+	__asm
+	{
+		add esp, 4
+		push shader
+		call[midAsmHookFunctionAddress]
+		jmp[midAsmHookReturnAddress]
+	}
+}
+
+extern "C" __declspec(dllexport) void Init()
+{
+	WRITE_JUMP(0x742516, replaceShader);
+}
