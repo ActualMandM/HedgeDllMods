3974cc40698dfa8dc5f71da4a46dbbc67ac23b4b
brianuuu <38166666+brianuuu@users.noreply.github.com>
2022-06-22 21:52:35 +0100

    [Colors Sonic] Randomized start crouching animation

--- Source/bb/ColorsSonic/ColorsPhysics.h
+++ Source/bb/ColorsSonic/ColorsPhysics.h
@@ -1,8 +1,36 @@
 #pragma once
 
+struct StartSoundData
+{
+	float m_soundTiming;	// ms
+	uint32_t m_cueID;
+	bool m_played;
+
+	void reset()
+	{
+		m_played = false;
+	}
+};
+
+struct StartData
+{
+	std::string m_animationName;
+	std::vector<StartSoundData> m_soundData;
+
+	void reset()
+	{
+		for (StartSoundData& data : m_soundData)
+		{
+			data.reset();
+		}
+	}
+};
+
 class ColorsPhysics
 {
 public:
 	static void applyPatches();
+
+	static std::vector<StartData> m_startCrouchingData;
 };
 
