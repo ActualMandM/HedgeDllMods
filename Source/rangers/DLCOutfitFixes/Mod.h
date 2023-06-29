bc3ef187bb92facf79604a4afddcdb8be2f27e24
Michael <15317421+ActualMandM@users.noreply.github.com>
2023-06-29 15:37:59 -0700

    [Sonic Outfit Fixes] Add outfit names as comments.

--- Source/rangers/DLCOutfitFixes/Mod.h
+++ Source/rangers/DLCOutfitFixes/Mod.h
@@ -5,14 +5,14 @@
 inline const int MAX_OUTFIT = 8;
 
 const char* dlcNames[MAX_OUTFIT][2] = {
-	{ "dlcddx", "explorer" },
-	{ "dlcmhr", "hunter" },
-	{ "dlcmhr", "airou" },
-	{ "dlcholoA", "korone" },
-	{ "dlccrm", "advshoes" },
-	{ "dlcxmas", "xmas" },
-	{ "chr", "birthday" },
-	{ "chr", "crown" }
+	{ "dlcddx", "explorer" },	// Adventurer's Gloves and Shoes
+	{ "dlcmhr", "hunter" },		// Rathalos
+	{ "dlcmhr", "airou" },		// Felyne Rathalos
+	{ "dlcholoA", "korone" },	// Inugami Korone Collaboration Sonic Gloves & Shoes
+	{ "dlccrm", "advshoes" },	// Sonic Adventure 2 Shoes
+	{ "dlcxmas", "xmas" },		// Holiday Cheer Suit
+	{ "chr", "birthday" },		// Sonic's Birthday Party Outfit
+	{ "chr", "crown" }			// Frontier Elite
 };
 
 const char* GetModelName(uint8_t outfit, const char* model, const char* type)
